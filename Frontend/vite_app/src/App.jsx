fd881becc0f5c37330a9cd52007e5b80781a5fdf
Jatin <jatinrajvani.cg@gmail.com>
2026-04-05 01:41:27 +0530

    Updating

--- Frontend/vite_app/src/App.jsx
+++ Frontend/vite_app/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Navigate, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Login from './pages/Auth/Login';
-import Signup from './pages/Auth/Signup';
+import Signup from './pages/Auth/SignUp';
 import Dashboard from './pages/Dashboard';
 import AnalyzeCall from './pages/AnalyzeCall';
 import CallList from './pages/CallList';
